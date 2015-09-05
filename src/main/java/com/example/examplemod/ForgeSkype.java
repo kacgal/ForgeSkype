860cd83ee8447eb2ae409f25f0f8250daebeb8a6
kacgal <kacgal@gmail.com>
2015-09-05 16:08:52 +0200

    Initial edits

--- src/main/java/com/example/examplemod/ForgeSkype.java
+++ src/main/java/com/example/examplemod/ForgeSkype.java
@@ -5,10 +5,9 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 
-@Mod(modid = ExampleMod.MODID, version = ExampleMod.VERSION)
-public class ExampleMod
-{
-    public static final String MODID = "examplemod";
+@Mod(modid = ForgeSkype.MODID, version = ForgeSkype.VERSION)
+public class ForgeSkype {
+    public static final String MODID = "forgeskype";
     public static final String VERSION = "1.0";
     
     @EventHandler
