c4a5cee4b5df118329bf741799df98acefe43ca8
kacgal <kacgal@gmail.com>
2015-09-15 21:42:47 +0200

    Bump version

--- src/main/java/com/kacgal/forgeskype/ForgeSkype.java
+++ src/main/java/com/kacgal/forgeskype/ForgeSkype.java
@@ -27,7 +27,7 @@ import java.util.List;
 @Mod(modid = ForgeSkype.MODID, version = ForgeSkype.VERSION, name = ForgeSkype.NAME)
 public class ForgeSkype {
     public static final String MODID = "forgeskype";
-    public static final String VERSION = "1.2.2";
+    public static final String VERSION = "1.2.3";
     public static final String NAME = "Skype for Forge";
 
     private static File customNamesFile = new File("skypecustomnames.txt");
